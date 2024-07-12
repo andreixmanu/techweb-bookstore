6ab447c242214a116fda8ceaa64a226530a9fa15
Andrei Manu <896868@stud.unive.it>
2024-07-12 11:19:34 +0200

    fixed getAllBooks endpoint

--- backend/routes/bookRoutes.ts
+++ backend/routes/bookRoutes.ts
@@ -6,9 +6,10 @@ const router = Router();
 // GET handler for /book
 router.get('/', testBook);
 router.post('/new', createBook);
+router.get('/all', getAllBooks)
 router.get('/:id', getBook);
 router.put('/:id', modifyBook);
 router.delete('/:id', deleteBook);
-router.get('/all', getAllBooks)
+
 
 export default router;
